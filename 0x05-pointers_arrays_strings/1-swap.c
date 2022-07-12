2a12708c8e71a3ab38562a6a0f5dd1654279d0ea
Daniel Love Amui <dannyloveamui@gmail.com>
2022-07-11 21:11:54 -0700

    swap values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * * swap_int - Changing value in memory with pointers
+ * * @a: Pointer to int a location
+ * * @b: Pointer to int b location
+ * *Return: Null/Void
+ * **/
 void swap_int(int *a, int*b){
 	int p = *a;
 	int q = *b;
