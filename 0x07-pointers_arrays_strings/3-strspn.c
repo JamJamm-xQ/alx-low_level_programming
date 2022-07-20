bf2f0cc67ea4f45a911a32dd8caf306303c66b60
Daniel Love Amui <dannyloveamui@gmail.com>
2022-07-19 19:20:56 -0700

    A file that contains the password for the crackme2 executable.

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _strspn - gets the length of a prefix substring
+ * _strspn - function that gets the length of a prefix substring
  * @s: Pointer to string location
  * @accept: bytes to filter
  * Return: Pointer to memory s
- **/
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0;
