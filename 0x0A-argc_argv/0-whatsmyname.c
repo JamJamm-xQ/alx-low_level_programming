79433a6e0918fd03145ca691b6c77b6f957f0579
Daniel Love Amui <dannyloveamui@gmail.com>
2022-07-27 20:09:46 -0700

    A program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
 * @argv: array of pointers to CLI arguement
 * Return: 0
 **/
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
